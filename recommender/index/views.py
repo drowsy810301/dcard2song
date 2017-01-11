de622e37ff223e8301fb1850ab50092cd317892b
drowsy810301 <lbj39919538@gmail.com>
2017-01-11 14:51:12 +0800

    init youtube api

--- recommender/index/views.py
+++ recommender/index/views.py
@@ -5,7 +5,7 @@ from gensim import models
 from gensim.models import word2vec
 #from index.recomm import model
 from index.recomm import find_song
-
+from index.apikey import api
 def index(request):
     article = ''
     #word = u'自由'
@@ -16,6 +16,6 @@ def index(request):
         #print len(model[word])
         #song_list = find_song(model)
         song_list = find_song(article['content'])
-        return render(request, "index.html", {'article': article, 'song_list': song_list})
+        return render(request, "index.html", {'article': article, 'song_list': song_list, 'api': api})
 
     return render(request, "index.html", {'article': article})
